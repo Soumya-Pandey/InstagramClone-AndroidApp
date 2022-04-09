682ce6e9201fdaa3c3f548c479c3fe4ed4e7a48c
Soumya Pandey <68410398+Soumya-Pandey@users.noreply.github.com>
2022-04-09 11:25:27 +0530

    signup java mods

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.instagramclone;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.instagramclone.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
     }
+
+    private void init(){
+        binding.loginn.setOnClickListener(v -> {
+            if((binding.username.getText().toString().equals("user")) & binding.password.getText().toString().equals("user")){
+                Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                Toast.makeText(MainActivity.this, "enter details", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
